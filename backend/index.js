8f31d901022c79b9ea0c0769cf1792b502b50f9f
Kaushal Mahajan <132477134+kaushal2023pro@users.noreply.github.com>
2026-01-19 17:53:00 +0530

    Update index.js
    added backend link

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://192.168.31.247:3000", "http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:3000", "http://192.168.31.247:3000", "http://localhost:5173", "http://localhost:5174","https://smartbuyai-1.onrender.com"],
     credentials: true
 }));
 
